5c6ba04382ef8b29c35fe2bc152ec144d6637025
Richard Henry Rozario <richardrozario.rr@gmail.com>
2020-12-08 19:23:01 +0530

    navbar and sidebar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,16 @@
 import React from "react";
-
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
 
 const Navbar = () => {
   return (
@@ -8,7 +18,26 @@ const Navbar = () => {
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">dolla</NavLogo>
-          <h1>Hey</h1>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="signin">Sign In</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
